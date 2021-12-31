5b2e0dff6c591ae224932a585780a15c98a5faf5
Minashraf <46136768+Minashraf@users.noreply.github.com>
2021-12-31 17:38:13 +0200

    Update sentence_extractor.py

--- tools/sentence_extractor.py
+++ tools/sentence_extractor.py
@@ -1,36 +1,13 @@
 import json
 
 # # --- For MIMIC dataset ---
-# dataset_dir = '/home/hoang/Datasets/MIMIC/'
-# section_tgt = 'FINDINGS:'
+dataset_dir = '/content/share/'
+section_tgt = 'FINDINGS:'
 
-# reports = json.load(open(dataset_dir + 'reports.json', 'r'))
-
-# count_sentence = {}
-# for file_name, report in reports.items():
-#     if section_tgt in report and report[section_tgt] != '':
-#         paragraph = report[section_tgt]
-#         sentences = paragraph.split('.')
-#         for sentence in sentences:
-#             sentence = sentence.strip()
-#             if sentence not in count_sentence:
-#                 count_sentence[sentence] = 1
-#             else:
-#                 count_sentence[sentence] += 1
-
-# count_sentence
-# json.dump(count_sentence, open(dataset_dir + 'count_sentence.json', 'w'))
-
-
-# --- For OpenI dataset ---
-dataset_dir = '../open-i/'
-section_tgt = 'FINDINGS'
-
-reports = json.load(open(dataset_dir + 'reports_ori.json', 'r'))
+reports = json.load(open(dataset_dir + 'reports.json', 'r'))
 
 count_sentence = {}
 for file_name, report in reports.items():
-    report = report['report']
     if section_tgt in report and report[section_tgt] != '':
         paragraph = report[section_tgt]
         sentences = paragraph.split('.')
@@ -41,4 +18,27 @@ for file_name, report in reports.items():
             else:
                 count_sentence[sentence] += 1
 
+count_sentence
 json.dump(count_sentence, open(dataset_dir + 'count_sentence.json', 'w'))
+
+
+# --- For OpenI dataset ---
+# dataset_dir = '../open-i/'
+# section_tgt = 'FINDINGS'
+
+# reports = json.load(open(dataset_dir + 'reports_ori.json', 'r'))
+
+# count_sentence = {}
+# for file_name, report in reports.items():
+#     report = report['report']
+#     if section_tgt in report and report[section_tgt] != '':
+#         paragraph = report[section_tgt]
+#         sentences = paragraph.split('.')
+#         for sentence in sentences:
+#             sentence = sentence.strip()
+#             if sentence not in count_sentence:
+#                 count_sentence[sentence] = 1
+#             else:
+#                 count_sentence[sentence] += 1
+
+# json.dump(count_sentence, open(dataset_dir + 'count_sentence.json', 'w'))
